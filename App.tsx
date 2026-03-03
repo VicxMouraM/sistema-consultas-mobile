3145419554eee1c7857f738a0197999bc555d5d6
Victoria Moura <vicxmoura@gmail.com>
2026-03-03 09:14:55 -0300

    Implementando estilos no React Native

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.texto}>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffe600',
+    backgroundColor: '#FFFE02',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  texto: {
+    color: "#FFFFF",
+    fontSize: 40,
+    fontWeight: "bold",
+  }
 });
